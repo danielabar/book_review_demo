9efd9585b3cfbd3158e90830308d9fb6a5ba239d
Daniela Baron <baron.daniela@gmail.com>
2025-07-05 09:35:10 -0400

    create some variety in seeded review timestamps

--- db/seeds/reviews.rb
+++ db/seeds/reviews.rb
@@ -7,13 +7,15 @@ books = Book.order("RANDOM()").limit(10)
 
 books.each do |book|
   reviewers = users.shuffle.sample(rand(1..[ users.size, 5 ].min))
-
   reviewers.each do |user|
+    some_date = rand(0..365).days.ago
     Review.create!(
       book: book,
       user: user,
       rating: rand(1..5),
-      body: Faker::Lorem.paragraph(sentence_count: 3)
+      body: Faker::Lorem.paragraph(sentence_count: 3),
+      created_at: some_date,
+      updated_at: some_date
     )
   end
 end
